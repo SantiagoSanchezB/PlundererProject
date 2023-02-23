a8c7bde279248982a2954b0ce49996ec8ed90a49
SantiagoSanchezB <santiago.sanchezda@gmail.com>
2023-02-23 15:43:28 -0500

    Add logo from Plunderer and change navbar

--- src/components/Organisms/Header.jsx
+++ src/components/Organisms/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../../assets/css/index_css.css';
+import Logo from '../../assets/img/Plunderer_Logo.png';
 
 const Header = () => (
     <div className = "">
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container-fluid">
+                <img src={Logo} class= "Logo"alt="" />
                 <a class="navbar-brand" href="/">Plunderer</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -12,22 +14,18 @@ const Header = () => (
                 <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/">Inicio</a>
+                        <a class="nav-link" aria-current="page" href="/">Index</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/Personajes">Personajes</a>
+                        <a class="nav-link" href="/Personajes">Characters</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/Contacto">Contacto</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/API">API-Plunderer</a>
+                        <a class="nav-link" href="/Contacto">Contact</a>
                     </li>
                 </ul>
                 </div>
             </div>
         </nav>
-        <h1 class="Name">Plunderer</h1>
     </div>
 );
 
